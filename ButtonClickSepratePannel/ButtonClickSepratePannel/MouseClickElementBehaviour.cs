59af61c02f755930d1246fec84a45a6923c60c95
Rahul Kumar <mailtorahulkrs@gmail.com>
2020-04-23 19:49:49 +0530

    CompleteAssessment

---
+++ ButtonClickSepratePannel/ButtonClickSepratePannel/MouseClickElementBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ButtonClickSepratePannel
+{
+   public class MouseClickElementBehaviour
+    {
+		public static readonly DependencyProperty HiddenUiElementProperty =
+			DependencyProperty.RegisterAttached("HiddenUiElement",
+				typeof(UIElement), typeof(MouseClickElementBehaviour),
+				new PropertyMetadata(null, OnHiddenUiElementChanged));
+
+		public static UIElement GetHiddenUiElement(UIElement uiElement)
+		{
+			return (UIElement)uiElement.GetValue(HiddenUiElementProperty);
+		}
+
+		public static void SetHiddenUiElement(UIElement uiElement, UIElement value)
+		{
+			uiElement.Visibility = Visibility.Collapsed;
+			uiElement.SetValue(HiddenUiElementProperty, value);
+		}
+
+		private static void OnHiddenUiElementChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			var uiElement = (UIElement)sender;
+			if (e.OldValue != null) GetVisibilityTimer((UIElement)e.OldValue).Dispose();
+
+			if (e.NewValue == null)
+			{
+				uiElement.MouseLeftButtonDown -= MouseDownEvent;
+				SetVisibilityTimer(uiElement, null);
+			}
+			else if (e.OldValue == null)
+			{
+				var hiddenUiElement = (UIElement)e.NewValue;
+				hiddenUiElement.Visibility = Visibility.Collapsed;
+				uiElement.MouseLeftButtonDown += MouseDownEvent;
+				var visibilityTimer = new PanelControlTimer(hiddenUiElement);
+				SetVisibilityTimer(uiElement, visibilityTimer);
+			}
+		}
+
+		private static void MouseDownEvent(object sender, RoutedEventArgs e)
+		{
+			var hiddenUiElement = GetHiddenUiElement((UIElement)sender);
+			hiddenUiElement.Visibility = (hiddenUiElement.Visibility == Visibility.Collapsed)
+				? Visibility.Visible : Visibility.Collapsed;
+			if (hiddenUiElement.Visibility == Visibility.Visible)
+				GetVisibilityTimer((UIElement)sender).Start();
+			else GetVisibilityTimer((UIElement)sender).Stop();
+		}
+
+		private static readonly DependencyProperty VisibilityTimerProperty =
+			DependencyProperty.RegisterAttached("VisibilityTimer",
+				typeof(PanelControlTimer), typeof(MouseClickElementBehaviour), new PropertyMetadata(null));
+
+		private static PanelControlTimer GetVisibilityTimer(UIElement uiElement)
+		{
+			return (PanelControlTimer)uiElement.GetValue(VisibilityTimerProperty);
+		}
+
+		private static void SetVisibilityTimer(UIElement uiElement, PanelControlTimer value)
+		{
+			uiElement.SetValue(VisibilityTimerProperty, value);
+		}
+	}
+}
---
+++ ButtonClickSepratePannel/ButtonClickSepratePannel/PanelControlTimer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace ButtonClickSepratePannel
+{
+    internal class PanelControlTimer :  IDisposable
+    {
+		private readonly UIElement _hiddenUiElement;
+		private readonly DispatcherTimer _timer = new DispatcherTimer();
+
+		public PanelControlTimer(UIElement hiddenUiElement)
+		{
+			_hiddenUiElement = hiddenUiElement;
+			_timer.Interval = new TimeSpan(0, 0, 10);
+			_timer.Tick += (s, arg) =>
+			{
+				//FadeOutStoryBoard(_hiddenUiElement);
+				_timer.Stop();
+			};
+			hiddenUiElement.MouseEnter += HiddenUiElementMouseEnter;
+			hiddenUiElement.MouseLeave += HiddenUiElementMouseLeave;
+		}
+
+		public void Start()
+		{
+			_timer.Start();
+			_hiddenUiElement.Visibility = Visibility.Visible;
+		}
+
+		public void Stop()
+		{
+			_timer.Stop();
+		}
+
+		private void HiddenUiElementMouseLeave(object sender, MouseEventArgs e) { Start(); }
+
+		private void HiddenUiElementMouseEnter(object sender, MouseEventArgs e) { Stop(); }
+
+		public void Dispose()
+		{
+			_hiddenUiElement.MouseEnter += HiddenUiElementMouseEnter;
+			_hiddenUiElement.MouseLeave += HiddenUiElementMouseLeave;
+		}
+
+		
+	}
+}
